c9808f3d5487c6ae14189ed7add0780c8488638a
Guilherme Viana <guilherme.fontes.dev@gmail.com>
2026-06-01 18:16:39 -0300

    Criação do Modelo de Entidade (Departamento) e teste de inserção

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1,"Computadores");
+		
+		System.out.println(obj);
+
+	}
+
+}
