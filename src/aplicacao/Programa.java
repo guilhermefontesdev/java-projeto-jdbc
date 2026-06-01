a78a57a522daa25b7ef95088f2f4e4ebd2f5b9f1
Guilherme Viana <guilherme.fontes.dev@gmail.com>
2026-06-01 18:29:11 -0300

    Adição da Classe Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,24 @@
 package aplicacao;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		
+		
 		Departamento obj = new Departamento(1,"Computadores");
 		
 		System.out.println(obj);
+		
+		
+		Vendedor vendedor = new Vendedor(3, "Jonas", "Jonas@gmail.com" , LocalDate.parse("22/06/2007" , DateTimeFormatter.ofPattern("dd/MM/yyyy")) , 3000.00, obj);
+		System.out.println(vendedor);
 
 	}
 
---
+++ src/modelo/entidades/Vendedor.java
@@ -0,0 +1,105 @@
+package modelo.entidades;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Vendedor implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	private String email;
+	private LocalDate aniversario;
+	private Double salarioBase;
+	private Departamento departamento;
+	
+	public Vendedor() {
+		
+	}
+
+	public Vendedor(Integer id, String nome, String email, LocalDate aniversario, Double salarioBase,
+			Departamento departamento) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.aniversario = aniversario;
+		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(LocalDate aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Vendedor [id=" + id + ", nome=" + nome + ", email=" + email + ", aniversario=" + aniversario
+				+ ", salarioBase=" + salarioBase + ", departamento=" + departamento + "]";
+	}
+	
+	
+	
+}
